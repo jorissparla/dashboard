5e22eea88b4600587c56b71601b874a5d3fa315d
Joris Sparla <joris.sparla@infor.com>
2016-12-14 21:12:47 +0100

    change

--- src/charts/appchartcontainer.js
+++ src/charts/appchartcontainer.js
@@ -1,67 +1,71 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React from 'react'
+import {connect} from 'react-redux'
 import {fetchSummary} from '../actions/index'
 import SummaryChart from './summarychart'
 
+const { func, string, number, object } = React.PropTypes
 
-class AppChartContainer extends Component {
-
-
-
-    myTimer() {
-        const data  = (!this.props.data) ? ['Logistics']: this.props.data ;
-        this.setState({
-            index: this.state.index + 1,
-            nrTeams: data.length
-        })
-
-        const {nrTeams, index} = this.state;
-        if (index > nrTeams - 1) {
-        this.setState({
-            index: 0
-        })  
-        }
+const AppChartContainer = React.createClass({
+  propTypes: {
+    fetchSummary: func,
+    data: object,
+    refreshRate: number,
+    value: string,
+    title: string,
+    type: string,
+    summary: string
+  },
+  myTimer () {
+    const data = (!this.props.data) ? ['Logistics'] : this.props.data
+    this.setState({
+      index: this.state.index + 1,
+      nrTeams: data.length
+    })
+    const {nrTeams, index} = this.state
+    if (index > nrTeams - 1) {
+      this.setState({
+        index: 0
+      })
     }
-    
-    componentWillMount () {
-          this.setState({ index: 0 })
-    }
-    
+  },
 
-    componentDidMount() {
-        this.props.fetchSummary();
-      
-        this.timerhandle = setInterval(this.myTimer.bind(this), this.props.refreshRate||15000)
+  componentWillMount () {
+    this.setState({ index: 0 })
+  },
 
-    }
+  componentDidMount () {
+    this.props.fetchSummary()
 
-    componentWillUnmount() {
-        clearInterval(this.timerhandle)
-    }
+    this.timerhandle = setInterval(this.myTimer, this.props.refreshRate || 15000)
+  },
 
+  componentWillUnmount () {
+    clearInterval(this.timerhandle)
+  },
 
-    render () {
-        const data  = (!this.props.data) ? ['Logistics']: this.props.data ;
-        const value = (!this.props.value) ? 'supportBacklog': this.props.value;
-        const title = (!this.props.title) ? value: this.props.title;
-        const type = (!this.props.type) ? "line" : this.props.type;
-        const team = data[this.state.index||0];
-        const summary= this.props.summary; //.reverse();
-        return (
-            <div>
-            <SummaryChart  data={summary}
-                title={title}
-                type={type}
-                xvalue="weekNr"
-                value={value}
-                team={team}/>
-            </div>
-        )
-    }
-}
+  render () {
+    const data = (!this.props.data) ? ['Logistics'] : this.props.data
+    const value = (!this.props.value) ? 'supportBacklog' : this.props.value
+    const title = (!this.props.title) ? value : this.props.title
+    const type = (!this.props.type) ? 'line' : this.props.type
+    const team = data[this.state.index || 0]
+    const summary = this.props.summary // .reverse()
+    console.dir(summary)
+    return (
+      <div>
+        <SummaryChart data={summary}
+          title={title}
+          type={type}
+          xvalue='weekNr'
+          value={value}
+          team={team} />
+      </div>
+    )
+  }
+})
 
 const mapStateToProps = (state) => {
-    return {summary: state.summary.summary}
+  return {summary: state.summary.summary}
 }
 
-export default connect(mapStateToProps, {fetchSummary})(AppChartContainer);
+export default connect(mapStateToProps, {fetchSummary})(AppChartContainer)
--- src/dashboardstats.js
+++ src/dashboardstats.js
@@ -1,50 +1,50 @@
-import React from 'react';
+import React from 'react'
 
-import './App.css';
+import './App.css'
 import AppChartCombi from './charts/appchartcombi'
 import AlertWidget from './alerts/alertwidget'
 import AppChartContainer from './charts/appchartcontainer'
 import HistoryChartContainer from './charts/historycontainer'
 
 const DashBoardStats = () => {
-    return (
-        <div className="row">
-            <div className="col s2">
-                <AlertWidget/>
-            </div>
-            <div className="col s10">
-                <div className="row">
-                    <AppChartContainer
-                        data={['Tools', 'Logistics', 'Finance']}
-                        refreshRate={16000}/>
-                    <AppChartContainer
-                        data={['Tools', 'Logistics', 'Finance']}
-                        value="surveyScore"
-                        title="Survey"
-                        type="column"
-                        refreshRate={11000}/>
-                    <HistoryChartContainer refreshRate={16000}/>
+  return (
+    <div className='row'>
+      <div className='col s2'>
+        <AlertWidget />
+      </div>
+      <div className='col s10'>
+        <div className='row'>
+          <AppChartContainer
+            data={['Tools', 'Logistics', 'Finance']}
+            refreshRate={16000} />
+          <AppChartContainer
+            data={['Tools', 'Logistics', 'Finance']}
+            value='surveyScore'
+            title='Survey'
+            type='column'
+            refreshRate={11000} />
+          <HistoryChartContainer refreshRate={16000} />
 
-                    <AppChartCombi
-                        title="Surveys"
-                        type="column"
-                        value="surveyScore"
-                        color="#9575cd"
-                        team="Logistics"/>
-                    <AppChartCombi
-                        title="Input"
-                        type="area"
-                        value="opened"
-                        color="#ffb300'"
-                        team="Logistics"/>
-
-                </div>
-            </div>
+          <AppChartCombi
+            title='Surveys'
+            type='column'
+            value='surveyScore'
+            color='#9575cd'
+            team='Logistics' />
+          <AppChartCombi
+            title='Input'
+            type='area'
+            value='opened'
+            color='#ffb300'
+            team='Logistics' />
 
         </div>
+      </div>
 
-    );
+    </div>
+
+  )
 }
-// <AppChartPie data={[1,2,3]} title="Input" type="pie" value="supportBacklog"
-// color="#ffb300'" team="Logistics"/>
-export default DashBoardStats;
+// <AppChartPie data={[1,2,3]} title='Input' type='pie' value='supportBacklog'
+// color='#ffb300'' team='Logistics'/>
+export default DashBoardStats
