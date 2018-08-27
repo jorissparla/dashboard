badc040838d4a1db30b3b92f679d06eb27db727f
Joris Sparla <joris.sparla@gmail.com>
2018-08-27 08:39:35 +0200

    added short id tp Dashboard stats new

--- src/DashBoardStatsNew.js
+++ src/DashBoardStatsNew.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import SummaryChartContainer from "./charts/SummaryChartContainer";
 import styled from "styled-components";
+import shortid from "shortid";
 
 const Row = styled.div`
   margin-left: 10px;
@@ -29,6 +30,7 @@ class DashBoardStats extends React.Component {
     return (
       <Row>
         <SummaryChartContainer
+          id={shortid.generate()}
           team={this.getTeam()}
           title="Support Backlog"
           refreshRate={0}
@@ -36,6 +38,7 @@ class DashBoardStats extends React.Component {
           color="#555555"
         />
         <SummaryChartContainer
+          id={shortid.generate()}
           team={this.getTeam()}
           value="surveyScore"
           title="Survey"
@@ -43,6 +46,7 @@ class DashBoardStats extends React.Component {
           refreshRate={0}
         />
         <SummaryChartContainer
+          id={shortid.generate()}
           team={this.getTeam()}
           value="escalated"
           title="Escalations"
@@ -52,6 +56,7 @@ class DashBoardStats extends React.Component {
         />
 
         <SummaryChartContainer
+          id={shortid.generate()}
           team={this.getTeam()}
           value="opened"
           title="Opened"
@@ -60,6 +65,7 @@ class DashBoardStats extends React.Component {
           refreshRate={0}
         />
         <SummaryChartContainer
+          id={shortid.generate()}
           team={this.getTeam()}
           value="Closed"
           title="Closed"
@@ -68,6 +74,7 @@ class DashBoardStats extends React.Component {
           refreshRate={0}
         />
         <SummaryChartContainer
+          id={shortid.generate()}
           team={this.getTeam()}
           value="chatpct"
           title="Chat %"
@@ -79,5 +86,6 @@ class DashBoardStats extends React.Component {
     );
   }
 }
+// added short id
 
 export default DashBoardStats;
