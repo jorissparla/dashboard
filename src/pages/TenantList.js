a4aa10a52638751420c2365c4fe748a52e27f389
Joris Sparla <joris.sparla@gmail.com>
2019-01-22 09:28:21 +0100

    refactor, delete some files and move tenantlist

--- src/pages/TenantList.js
+++ src/pages/TenantList.js
@@ -10,7 +10,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import SearchBar from './common/SearchBar';
+import SearchBar from '../common/SearchBar';
 import Chip from '@material-ui/core/Chip';
 import format from 'date-fns/format';
 
