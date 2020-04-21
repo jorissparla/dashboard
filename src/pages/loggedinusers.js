6f1337d838f4e337b46e4a6f4c89e4941d3ede47
Joris Sparla <joris.sparla@infor.com>
2020-04-21 21:42:38 +0200

    addhours GMT+2

--- src/pages/loggedinusers.js
+++ src/pages/loggedinusers.js
@@ -2,7 +2,7 @@ import React from "react";
 import useSWR from "swr";
 import { request } from "graphql-request";
 import { uri } from "../index";
-import { format } from "date-fns";
+import { format, addHours } from "date-fns";
 import _ from "lodash";
 
 const query = `
@@ -31,7 +31,7 @@ const Loggedinusers = () => {
           <div className="border rounded shadow-lg bg-white p-2 m-2 w-full ">
             <h1 className="font-pop  font-semibold overflow-hidden">{acc.fullname}</h1>
             <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-teal-200 text-teal-800">
-              {format(parseInt(acc.lastlogin), "yyyy-MM-dd HH:mm")}
+              {format(addHours(parseInt(acc.lastlogin), -2), "yyyy-MM-dd HH:mm")}
             </span>
           </div>
         ))}
