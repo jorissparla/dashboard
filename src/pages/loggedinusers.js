cbe4571454d054b3334daa1475687e3acaa480f9
Joris Sparla <joris.sparla@infor.com>
2020-04-22 09:27:24 +0200

    loggedinusers search

--- src/pages/loggedinusers.js
+++ src/pages/loggedinusers.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
 import { request } from "graphql-request";
 import { uri } from "../index";
 import { format, addHours } from "date-fns";
 import _ from "lodash";
+import SearchBar from "common/SearchBar";
 
 const query = `
   {
     accounts {
     fullname
     lastlogin
+    image
   }
 }
 
@@ -22,15 +24,24 @@ const Loggedinusers = () => {
     (query) => request(uri, query)
   );
 
+  const [searchText, setSearchText] = useState("");
   let showData = data?.accounts.filter((d) => d.lastlogin);
+  if (searchText) {
+    showData = showData.filter(({ fullname }) => fullname.includes(searchText));
+  }
   showData = _.orderBy(showData, "lastlogin", "desc");
+
   return (
     <div className="h-screen w-full bg-gray-200">
-      <div className="grid grid-cols-5 gap-1">
+      <SearchBar onChange={(value) => setSearchText(value)} hintText="Enter name" />
+      <div className="grid lg:grid-cols-7 sm:grid-cols-6 gap-1 mx-auto w-full">
         {showData?.map((acc) => (
-          <div className="border rounded shadow-lg bg-white p-2 m-2 w-full ">
-            <h1 className="font-pop  font-semibold overflow-hidden">{acc.fullname}</h1>
-            <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-teal-200 text-teal-800">
+          <div key={acc.fullname} className=" rounded shadow-lg bg-white p-2 m-2 w-full overflow-hidden">
+            <div className="flex items-center justify-between mb-2">
+              <h1 className="font-pop  font-semibold overflow-hidden">{acc.fullname}</h1>
+              <img className="h-12 w-12 shadow rounded-full" src={acc.image} alt="" />
+            </div>
+            <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-teal-200 text-teal-800">
               {format(addHours(parseInt(acc.lastlogin), -2), "yyyy-MM-dd HH:mm")}
             </span>
           </div>
