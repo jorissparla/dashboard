77af0e1f11c5aafb7bab2dc69cf403681e9f9b89
Joris Sparla <joris.sparla@infor.com>
2020-04-22 11:47:45 +0200

    initials added

--- src/pages/loggedinusers.js
+++ src/pages/loggedinusers.js
@@ -5,6 +5,7 @@ import { uri } from "../index";
 import { format, addHours } from "date-fns";
 import _ from "lodash";
 import SearchBar from "common/SearchBar";
+import getInitials from "tenants/details/getInitials";
 
 const query = `
   {
@@ -25,6 +26,7 @@ const Loggedinusers = () => {
   );
 
   const [searchText, setSearchText] = useState("");
+
   let showData = data?.accounts.filter((d) => d.lastlogin);
   if (searchText) {
     showData = showData.filter(({ fullname }) => fullname.includes(searchText));
@@ -34,18 +36,26 @@ const Loggedinusers = () => {
   return (
     <div className="h-screen w-full bg-gray-200">
       <SearchBar onChange={(value) => setSearchText(value)} hintText="Enter name" />
-      <div className="grid lg:grid-cols-7 sm:grid-cols-6 gap-1 mx-auto w-full">
-        {showData?.map((acc) => (
-          <div key={acc.fullname} className=" rounded shadow-lg bg-white p-2 m-2 w-full overflow-hidden">
-            <div className="flex items-center justify-between mb-2">
-              <h1 className="font-pop  font-semibold overflow-hidden">{acc.fullname}</h1>
-              <img className="h-12 w-12 shadow rounded-full" src={acc.image} alt="" />
+      <div className="w-full px-6">
+        <div className="grid lg:grid-cols-7 sm:grid-cols-6 gap-1 mx-auto w-full">
+          {showData?.map((acc) => (
+            <div key={acc.fullname} className=" rounded shadow-lg bg-white p-2 m-2 w-full overflow-hidden">
+              <div className="flex items-center justify-between mb-2">
+                <h1 className="font-pop  font-semibold overflow-hidden">{acc.fullname}</h1>
+                {acc.image ? (
+                  <img className="h-12 w-12 shadow rounded-full" src={acc.image} alt="Profile Picture" />
+                ) : (
+                  <div className="h-12 w-12 shadow rounded-full bg-teal-200 text-teal-800 text-xl flex items-center justify-center font-semibold">
+                    {getInitials(acc.fullname)}
+                  </div>
+                )}
+              </div>
+              <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-teal-200 text-teal-800">
+                {format(addHours(parseInt(acc.lastlogin), -2), "yyyy-MM-dd HH:mm")}
+              </span>
             </div>
-            <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-teal-200 text-teal-800">
-              {format(addHours(parseInt(acc.lastlogin), -2), "yyyy-MM-dd HH:mm")}
-            </span>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       {/* <pre>{JSON.stringify(showData, null, 2)}</pre> */}
       <pre>{JSON.stringify(error, null, 2)}</pre>
