91c316a37730cb6c874303440ef5981002ea823e
Joris Sparla <joris.sparla@infor.com>
2020-07-29 16:44:16 +0200

    updated navbar and upgraded tailwind css

--- src/projects/projectform.js
+++ src/projects/projectform.js
@@ -8,6 +8,7 @@ import { useAlert } from "globalState/AlertContext";
 import { useHistory } from "react-router";
 import CKEditor from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import SafeDeleteButton from "videos/SafeDeleteButton";
 
 const ACCOUNTS = gql`
   query ACCOUNTS {
@@ -117,6 +118,8 @@ const ProjectForm = ({ id = null }) => {
       history.push("/projects");
     }, 2000);
   };
+
+  const handleDelete = async () => {};
 
   const handleSave = async () => {
     if (validate()) {
@@ -334,7 +337,7 @@ const ProjectForm = ({ id = null }) => {
             <div className="col-span-3 mt-2 pt-3 border-t border-gray-100">
               <Keywords updateKeywords={handleKeywordsChange} id="1" keywords={values.keywords} readOnly={false} />
             </div>
-            <div className="flex align-items items-center space-between w-24">
+            <div className="flex align-items items-center space-between w-36">
               <TWButton color="teal" onClick={handleSave}>
                 {id ? "Update" : "Save"}
               </TWButton>
@@ -348,6 +351,25 @@ const ProjectForm = ({ id = null }) => {
                 Cancel
               </a>
             </div>
+            <span class="inline-flex rounded-md shadow-sm">
+              <button
+                type="button"
+                class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150 cursor-not-allowed"
+                disabled="disabled"
+              >
+                <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />{" "}
+                  <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  />
+                </svg>
+                Processing
+              </button>
+            </span>
+
+            <SafeDeleteButton onClick={handleDelete} />
           </div>
         </div>
       </div>
